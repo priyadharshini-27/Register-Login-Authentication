e061155782a3b5d1e0a18731a2d66e85a76b8292
Priyadharshini <61590245+priyadharshini-27@users.noreply.github.com>
2020-08-01 22:34:04 +0530

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -3,15 +3,15 @@ const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: True,
+    required: true,
   },
   email: {
     type: String,
-    required: True,
+    required: true,
   },
   password: {
     type: String,
-    required: True,
+    required: true,
   },
   date: {
     type: Date,
@@ -19,6 +19,6 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
-const user = mongoose.model("User", UserSchema);
+const User = mongoose.model("User", UserSchema);
 
-module.exports = user;
+module.exports = User;
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+//user model
+const user = require("../models/User");
+const User = require("../models/User");
 
 //Login Page
 router.get("/login", (req, res) => res.render("login"));
@@ -21,19 +25,60 @@ router.post("/register", (req, res) => {
   }
 
   //check password match
-  if (password1 != password2) {
+  if (password != password2) {
     err.push({ msg: "Passwords do not match" });
   }
 
   //check password length
-  if(password.length<6){
-    err.push({msg:'Password should have minimum six characters'})
+  if (password.length < 6) {
+    err.push({ msg: "Password should have minimum six characters" });
   }
 
-  if(err.length>0){
-      res.render('register',{
-          errors
-      })
+  if (err.length > 0) {
+    res.render("register", {
+      err,
+      name,
+      email,
+      password,
+      password2,
+    });
+  } else {
+    //validation if the email already exists or not if not add to db
+    user.findOne({ email: email }).then((data) => {
+      if (data) {
+        err.push({ msg: "Email Id already exists" });
+        res.render("register", {
+          err,
+          name,
+          email,
+          password,
+          password2,
+        });
+      } else {
+        const newUser = new User({
+          name,
+          email,
+          password,
+        });
+        //console.log(newUser)
+        //res.send('hello');
+
+        //bcrypt password
+        const saltRounds = 10;
+        bcrypt.genSalt(saltRounds, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            // Now we can store the password hash in db.
+            if (err) throw err;
+            //set password to hased
+            newUser.password = hash;
+
+            newUser.save()
+            .then(user=>res.redirect('/user/login'))
+            .catch(err=>console.log(err));
+          });
+        });
+      }
+    });
   }
 });
 
