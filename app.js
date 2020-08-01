590773af803cbcbf6177340ee9d975f615141222
Priyadharshini <61590245+priyadharshini-27@users.noreply.github.com>
2020-08-01 23:57:49 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
-const flash=require("connect-flash");
-const session=require('express-session');
+const flash = require("connect-flash");
+const session = require("express-session");
+const passport = require("passport");
 
 const app = express();
+
+//password config
+require("./config/passport")(passport);
 
 //db config
 const db = require("./config/keys").mongoURI;
@@ -23,21 +27,28 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 
 //express-session
-app.use(session({
-  secret: 'secret',
-  resave: true,
-  saveUninitialized: true
-}));
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 //connecting flash
 app.use(flash());
 
 //global variables
-app.use((req,res,next)=>{
-  res.locals.success_msg=req.flash('success_msg');
-  res.locals.error_msg=req.flash('error_msg');
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("message");
   next();
-})
+});
 
 //routers
 app.use("/", require("./routes/index"));
